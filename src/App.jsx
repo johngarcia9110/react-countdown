83a43aa4bcf2d5d7f85722d5466e094f8b1aadd9
johngarcia9110 <johngarcia9110@gmail.com>
2017-03-07 20:48:06 -0500

    added styles and state to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            deadline : 'aDecember 25, 2017',
+            newDeadLine : ''
+        }
+    }
+
+    changeDeadline(){
+        this.setState({deadline : this.state.newDeadLine});
+    }
+
     render(){
         return(
             <div className="app">
-                <div className="counter-title"> Countdown to: December 25, 2017</div>
+                <div className="counter-title"> Countdown to: {this.state.deadline}</div>
                 <div className="counter-list">
                     <div className="counter-item">14 days</div>
                     <div className="counter-item">7 hours</div>
@@ -13,8 +25,11 @@ class App extends Component{
                     <div className="counter-item">34 seconds</div>
                 </div>
                 <div className="counter-add">
-                    <input type="date" placeholder='enter date here'/>
-                    <button>Submit</button>
+                    <input 
+                    placeholder='enter date here'
+                    onChange={event => this.setState({newDeadLine : event.target.value})}
+                    />
+                    <button onClick={() => this.changeDeadline()}>Submit</button>
                 </div>
                 <div className="triangle"></div>
             </div>
